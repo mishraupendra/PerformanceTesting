9d106164939ca2c15b4bee4e31f39308bc081920
mishraupendra / PerformanceTesting <mishra94upendra@gmail.com>
2022-06-06 16:30:51 +0530

    Commit- 06/Jun

---
+++ basic_http_host01.py
@@ -0,0 +1,11 @@
+from locust import HttpUser,task,between
+
+
+class MyUser(HttpUser):
+
+    wait_time=between(1,2)
+    host = "http://newtours.demoaut.com/"
+
+    @task
+    def login_URL(self):
+        print("I am login into URL")
